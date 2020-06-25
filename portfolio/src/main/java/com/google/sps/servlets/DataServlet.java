6e7896fd3e644b5478087976fa06af591b8a711c
Mariah Crawford <sh.evergreen34@gmail.com>
2020-06-25 02:21:20 -0400

    Get uploaded file url via Blobstore and add an image property to Comment Entity

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -25,7 +35,9 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -51,7 +63,8 @@ public class DataServlet extends HttpServlet {
     for (Entity entity : results.asIterable()) {  
         String name = (String) entity.getProperty("user_name");
         String comment = (String) entity.getProperty("user_comment");
-        messages.add("\""+ comment + "\" \n - " + name);
+        String image = (String) entity.getProperty("user_img");
+        messages.add("\""+ comment + "\" \n - " + name + " img: " +image);
     } 
     
     // Convert the ArrayList to JSON
@@ -69,15 +82,53 @@ public class DataServlet extends HttpServlet {
     String name = request.getParameter("name-input");
     String comment = request.getParameter("comment-input");
 
-    // Create entity with name and comment properties
+    // Get the URL of the image that the user uploaded to Blobstore.
+    String imageUrl = getUploadedFileUrl(request, "image");
+
+    //Create entity with name and comment properties
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("user_name", name);
     commentEntity.setProperty("user_comment", comment);
+    commentEntity.setProperty("user_img", imageUrl);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
 
     // Redirect back to the HTML page.
-    response.sendRedirect("/index.jsp");
+    response.sendRedirect("/index.html");
+  }
+
+  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+
+    // User submitted form without selecting a file, so we can't get a URL. (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+    String url = imagesService.getServingUrl(options);
+
+    // The URL is relative to the current domain.
+    if(url.startsWith("https://8080-dot-12521393-dot-devshell.appspot.com/")){
+      url = url.replace("https://8080-dot-12521393-dot-devshell.appspot.com/", "/");
+    }
+    return url;
   }
 }
+
