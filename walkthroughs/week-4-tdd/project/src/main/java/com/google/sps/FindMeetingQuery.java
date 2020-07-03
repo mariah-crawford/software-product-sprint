a0321f67209bff80fcaebedb38dd4af7c8c46068
Mariah Crawford <sh.evergreen34@gmail.com>
2020-07-02 23:28:15 -0400

    return the times when a meeting could happen, given meeting information

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,69 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.List;
+import java.lang.*;
+import java.util.Comparator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> meetingTimes = new ArrayList<TimeRange>();
+
+    //Request information
+    long requestDuration = request.getDuration();
+    Collection<String> requestAttendees = request.getAttendees();
+
+    // Traverse collection of events to get attendees, and time range of each relevant event
+    for(Event event: events){
+        TimeRange eventRange = event.getWhen();
+
+        // Modify copy of Event Attendees to only include relevant attendees that overlap with the request
+        Set<String> eventAttendees = event.getAttendees();
+        Set<String> copy = new HashSet<>();
+        copy.addAll(eventAttendees);
+        copy.retainAll(requestAttendees);
+        
+        // If any of the attendees overlap with this event's attendees, then this duration is unavailable  
+        if (!(copy.isEmpty())){
+            meetingTimes.add(eventRange);
+        }
+    }
+
+    // Convert Collection of TimeRanges into a List in order to sort in ascending order
+    List<TimeRange> listMeetingTimes = new ArrayList<TimeRange>(meetingTimes);
+    Collections.sort(listMeetingTimes, TimeRange.ORDER_BY_START);
+
+    // Final Collection to hold available time ranges for the requested meeting
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+
+    int start = TimeRange.START_OF_DAY;
+    int end = TimeRange.END_OF_DAY;
+    
+    for(TimeRange range: listMeetingTimes){
+        // Create time range in between scheduled times if there is enough time allocated
+        if((range.start() > start) && ((range.start() - start) >= requestDuration)){
+            TimeRange timeTemp = TimeRange.fromStartEnd(start, range.start(), false);
+            availableTimes.add(timeTemp);
+            start = range.end();
+        }else{
+            // Ensures the latest end time for overlapping events
+            start = Math.max(start, range.end());
+        }
+    }
+
+    // Checks if there is enough time between the end of the last event in listMeetingTimes and the end of day to create a time range
+    if((start != end+1) && ((end+1 - start) >= requestDuration)){
+        TimeRange timeTemp = TimeRange.fromStartEnd(start, end, true);
+        availableTimes.add(timeTemp);
+    }
+
+    return availableTimes;
   }
 }
